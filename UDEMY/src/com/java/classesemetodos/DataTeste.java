4c9df74ba710dccc2fdf7b02607b46a419a8b844
Sabrina  Barsil <barsilsabrina@gmail.com>
2022-08-30 11:35:35 -0300

    implementando construtores

--- UDEMY/src/com/java/classesemetodos/DataTeste.java
+++ UDEMY/src/com/java/classesemetodos/DataTeste.java
@@ -3,45 +3,23 @@ package com.java.classesemetodos;
 public class DataTeste {
 
 	public static void main(String[] args) {
-		Data d1 = new Data();
-
-		d1.dia = 07;
-		d1.mes = 5;
-		d1.ano = 1991;
 		
-
-		Data d2 = new Data();
-
-		d2.dia = 17;
-		d2.mes = 1;
-		d2.ano = 2013;
+		Data d1 = new Data(); // constutor defaul
 		
+		Data d2 = new Data(17, 1, 2013);
 
-		Data d3 = new Data();
+		Data d3 = new Data(19, 5, 2015);
 
-		d3.dia = 19;
-		d3.mes = 5;
-		d3.ano = 2015;
-		
-
-		Data d4 = new Data();
-
-		d4.dia = 31;
-		d4.mes = 3;
-		d4.ano = 2020;
-		
+		Data d4 = new Data(31, 3, 2020);
 
 		Data d5 = new Data();
+		d5.ano=1988;
 
-		d5.dia = 04;
-		d5.mes = 8;
-		d5.ano = 1988;
-	
 
 		System.out.print("A Sabrina nasceu em " + d1.dataFormatada());
 		System.out.print("\nA Maria nasceu em " + d2.dataFormatada());
-		System.out.print("\nO Pedro nasceu em " + d3.dataFormatada() );
-		System.out.print("\nA Isis nasceu em " + d4.dataFormatada() );
+		System.out.print("\nO Pedro nasceu em " + d3.dataFormatada());
+		System.out.print("\nA Isis nasceu em " + d4.dataFormatada());
 		System.out.print("\nO Helton nasceu em " + d5.dataFormatada());
 
 	}
